507f1c4ee1d0bf8c8eaccaa02a565fad735a077e
Jamal SIDIKOU <sidikjam08@gmail.com>
2021-03-12 13:00:12 +0100

    React context api added

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useStateValue } from '../StateProvider';
 import './product.css'
 
 function Product({ id, title, image, price, rating }) {
+
+    const [{basket}, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        dispatch({
+            type:'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    };
     return (
         <div className="product">
             <div className="product__info">
@@ -22,7 +38,7 @@ function Product({ id, title, image, price, rating }) {
 
             </div>
             <img src={image} />
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
             
         </div>
     )
--- src/components/reducer.js
+++ src/components/reducer.js
@@ -1,14 +1,19 @@
 export const initialState= {
-    basket: ["chicken","bucket"],
+    basket: [],
 };
 
-function reducer(state,action) {
+const reducer= (state,action) => {
+    console.log(action)
     switch (action.type) {
         case 'ADD_TO_BASKET':
-            
+            return {
+                ...state,
+                basket: [...state.basket, action.item],
+            };
             break;
     
         case 'REMOVE_FROM_BASKET':
+            return {state};
             break;
         default:
             return state;
